239970bab38afde7c203c3787cc29177de8d5e30
Monjur Hossen <14monjurhossen@gmail.com>
2023-09-20 13:21:44 +0600

    update

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,9 +2,42 @@ import './Header.css'
 import heroimg from '../../assets/ai.webp'
 import Button from '../Button/Button'
 import grpimg from '../../assets/people.png'
+import {gsap} from "gsap"
+import {useRef, useEffect} from "react"
+
 const Header = () => {
+  let parent = useRef(null)
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      gsap.from(".header__left h1", {
+       x: -2000,
+       duration: 1.5,
+      })
+      gsap.from(".header__right", {
+        x: 2000,
+        duration: 1.5
+      })
+      gsap.from(".header__left>p", {
+        x: -2000,
+        duration: 2,
+        delay: 0.5
+      })
+      gsap.from(".input__div", {
+        x: -2000,
+        duration: 2,
+        delay: 1
+      })
+      gsap.from(".img_content", {
+        x: -2000,
+        duration: 2,
+        delay: 1.5
+      })
+    }, parent)
+    return () => ctx.revert();
+  },[])
+
   return (
-    <div className='header gradient_bg' id='home'>
+    <div className='header gradient_bg' id='home' ref={parent}>
       <div className='header__inner container'>
         <div className="header__left">
           <h1>Let’s Build Something amazing with GPT-3 OpenAI</h1>
